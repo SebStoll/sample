49d4c5e297e85662bd1fbbbc811b4915f5e4f101
Sebastian Stoll <seb_stoll@gmx.de>
2018-10-31 14:08:30 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='sample',
+    version='0.1',
+    description='Description of sample project.',
+    url='http://sampleurl',
+    author='Author of sample project',
+    author_email='author@email.com',
+    license='MIT',
+    packages=['sample'],
+    zip_safe=False
+)
